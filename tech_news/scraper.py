92adbb8243cc36e144f265fa811e79187a57b0b5
Isaque <zazac3179@gmail.com>
2023-09-01 13:02:01 -0300

    implementação da função scrape_next_page_link que é responsável por fazer o scrape do link da próxima página

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -29,8 +29,11 @@ def scrape_updates(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    selector = Selector(text=html_content)
+    next_page = selector.css("a.next.page-numbers::attr(href)").get()
+    if not next_page:
+        return None
+    return next_page
 
 
 # Requisito 4
