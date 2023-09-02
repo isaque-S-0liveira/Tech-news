b63ea550c2a23193ddffbaaae4ae765632ad67b6
Isaque <zazac3179@gmail.com>
2023-09-01 22:45:47 -0300

    implementação da função search_by_title que faz uma busca no DB com base no titulo passado

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,7 +1,14 @@
+from tech_news.database import db
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    regex_pattern = f".*{title}.*"
+    query = {"title": {"$regex": regex_pattern, "$options": "i"}}
+    news = db.news.find(query)
+    result = [(news_item["title"], news_item["url"]) for news_item in news]
+
+    return result
 
 
 # Requisito 8
